02abe5fcedf91d0502a04df738d2a48fff885542
Hikmatyar Hasan <hh2110@icloud.com>
2024-07-16 22:36:14 +0100

    feat: adding calendar and timeslots

--- app.py
+++ app.py
@@ -1,42 +1,106 @@
-from dash import Dash, callback, html, dcc
+from datetime import datetime as dt
+
+import dash
 import dash_bootstrap_components as dbc
-import numpy as np
-import pandas as pd
-import matplotlib as mpl
-import gunicorn                     #whilst your local machine's webserver doesn't need this, Heroku's linux webserver (i.e. dyno) does. I.e. This is your HTTP server
-from whitenoise import WhiteNoise   #for serving static files on Heroku
+from dash import dcc, html
+from dash.dependencies import Input, Output, State
 
-# Instantiate dash app
-app = Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-# Reference the underlying flask app (Used by gunicorn webserver in Heroku production deployment)
-server = app.server 
-
-# Enable Whitenoise for serving static files from Heroku (the /static folder is seen as root by Heroku) 
-server.wsgi_app = WhiteNoise(server.wsgi_app, root='static/') 
 
 # Define Dash layout
 def create_dash_layout(app):
-
     # Set browser tab title
-    app.title = "Your app title" 
-    
-    # Header
-    header = html.Div([html.Br(), dcc.Markdown(""" # Hi. I'm your Dash app."""), html.Br()])
-    
-    # Body 
-    body = html.Div([dcc.Markdown(""" ## I'm ready to serve static files on Heroku. Just look at this! """), html.Br(), html.Img(src='charlie.png')])
+    app.title = "Appointment Booking"
 
-    # Footer
-    footer = html.Div([html.Br(), html.Br(), dcc.Markdown(""" ### Built with ![Image](heart.png) in Python using [Dash](https://plotly.com/dash/)""")])
-    
-    # Assemble dash layout 
-    app.layout = html.Div([header, body, footer])
+    url = dcc.Location(id="url", refresh=False)
+
+    # Header
+    header = html.H5("Appointment Booking", className="display-3")
+    paragraph = html.P(
+        "Book your appointment easily for", className="lead", id="paragraph"
+    )
+
+    # Calendar
+    calendar = dbc.Form(
+        [
+            dbc.Label("Choose Date", html_for="date-picker"),
+            dcc.DatePickerSingle(
+                id="date-picker",
+                min_date_allowed=dt(1995, 8, 5),
+                max_date_allowed=dt(2017, 9, 19),
+                initial_visible_month=dt(2017, 8, 5),
+                date=str(dt(2017, 8, 25, 23, 59, 59)),
+            ),
+        ]
+    )
+
+    # Time slots
+    time_slots = dbc.Form(
+        [
+            dbc.Label("Choose Time Slot", html_for="time-slot"),
+            dcc.Dropdown(
+                id="time-slot",
+                options=[
+                    {"label": "9:00 - 10:00", "value": "9:00 - 10:00"},
+                    {"label": "10:00 - 11:00", "value": "10:00 - 11:00"},
+                    # Add more time slots as needed
+                ],
+                value="9:00 - 10:00",
+            ),
+        ]
+    )
+
+    # Submit button
+    submit_button = html.Button("Book Appointment", id="submit-button", n_clicks=0)
+
+    # Notification
+    notification = html.Div(id="notification")
+
+    # Assemble dash layout
+    app.layout = dbc.Container(
+        [header, url, paragraph, calendar, time_slots, submit_button, notification],
+        fluid=True,
+    )
 
     return app
 
-# Construct the dash layout
-create_dash_layout(app)
 
-# Run flask app
-if __name__ == "__main__": app.run_server(debug=False, host='0.0.0.0', port=8050)
+# Construct the dash layout
+app = create_dash_layout(app)
+
+
+# Define callback to handle button click
+@app.callback(
+    Output("notification", "children"),
+    [Input("submit-button", "n_clicks")],
+    [State("date-picker", "date"), State("time-slot", "value")],
+)
+def update_output(n_clicks, date, time_slot):
+    if n_clicks > 0:
+        return f"Appointment booked for {date} at {time_slot}!"
+    else:
+        return ""
+
+
+@app.callback(
+    Output("paragraph", "children"),
+    [Input("url", "pathname")],
+    suppress_callback_exceptions=True,
+)
+def update_paragraph(pathname):
+    # Extract the doctor parameter from the URL
+    try:
+        doctor, token = pathname[1:].split("&")
+    except ValueError:
+        doctor = None
+        token = None
+
+    if doctor:
+        return f"Book your appointment easily with {doctor=} for {token=}"
+    else:
+        return "No doctor selected"
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
